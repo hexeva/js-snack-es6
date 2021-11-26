2f8a4e21ce69dc3a04ca47cd093768238b0357d1
hexeva <patrick79.g@gmail.com>
2021-11-26 15:13:06 +0100

    starting snack 1

---
+++ snack-1/js/script.js
@@ -0,0 +1,73 @@
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal nel DOM
+
+
+// Creare un array di oggetti:
+const bikesArray = [
+
+
+    {
+        nome:'coppini',
+        pesoKg: 1.5
+    },
+
+    {
+        nome:'Atala',
+        pesoKg: 1.7
+    },
+
+    {
+        nome: 'specialized',
+        pesoKg: 1.3
+    },
+
+    {
+        nome:'Felt',
+        pesoKg: 1.4
+    },
+
+    {
+        nome:'Bottecchia',
+        pesoKg: 2
+    },
+
+    {
+        nome:'cicli Poccianti',
+        pesoKg: 2.4
+    },
+
+
+];
+
+// console.log(bikesArray);
+
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal nel DOM
+
+// creo una variabile assumendo che la prima dell'indice sia quella con peso minore ----> 
+    // se il peso di thisBikes < bikeWeight 
+    // allora bikeWeight = thisBikes
+
+let lessWeightBike = bikesArray[0];
+// console.log(lessWeightBike);
+
+
+// creo il ciclo for per scorrere l'array con tutti gli objects al suo interno
+
+for(let i = 0; i < bikesArray.length;i++){
+    // mi salvo l'indice dell'array in una variabile
+    let thisBikes = bikesArray[i];
+    // console.log(thisBikes);
+   
+
+    // stabilisco con una if quale bici è più leggera (quella che io ho stabilito che lo sia confrontandola con tutte quelle presenti nell'array)
+
+    if(thisBikes.pesoKg < lessWeightBike.pesoKg){
+        lessWeightBike = thisBikes;
+    }
+    console.log(lessWeightBike);
+    
+    
+}
+
+    // console.log(lessWeightBike);
