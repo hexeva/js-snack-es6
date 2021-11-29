bdd473c7fd8cb1440f64f7eb69d770c25c899835
hexeva <patrick79.g@gmail.com>
2021-11-29 15:16:13 +0100

    push for each version

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -1,94 +1,67 @@
-// Creare un array di oggetti:
-// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal nel DOM
+// Dato un elenco degli studenti di una facoltà, con il totale dei loro voti
+let students = [
+    { name: 'Marco', id: 213, grades: 78 },
+    { name: 'Paola', id: 110, grades: 96 },
+    { name: 'Andrea', id: 250, grades: 48 },
+    { name: 'Gaia', id: 145, grades: 74 },
+    { name: 'Luigi', id: 196, grades: 68 },
+    { name: 'Piero', id: 102, grades: 50 },
+    { name: 'Francesca', id: 120, grades: 84 },
+  ];
+// 1- Dobbiamo creare delle targhe con il loro nome in maiuscolo. Ci serve quindi un nuovo array di stringhe. Ogni elemento del nuovo array sarà il nome dello studente ma con tutte le lettere maiuscole.(map)
+// 2-  Dobbiamo creare un nuovo array con gli studenti che hanno un totale di voti superiore a 70 (filter)
+// 3-  Dobbiamo creare un nuovo array di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120 (filter)
 
 
-// Creare un array di oggetti:
-const bikesArray = [
 
+// 1- Dobbiamo creare delle targhe con il loro nome in maiuscolo. Ci serve quindi un nuovo array di stringhe. Ogni elemento del nuovo array sarà il nome dello studente ma con tutte le lettere maiuscole.(map)
 
-    {
-        nome:'coppini',
-        pesoKg: 1.5
-    },
+// versione con for each 
 
-    {
-        nome:'Atala',
-        pesoKg: 1.7
-    },
+const provaArray =[];
 
-    {
-        nome: 'specialized',
-        pesoKg: 0.8
-    },
-
-    {
-        nome:'Felt',
-        pesoKg: 1.4
-    },
-
-    {
-        nome:'Bottecchia',
-        pesoKg: 2
-    },
-
-    {
-        nome:'cicli Poccianti',
-        pesoKg: 2.4
-    },
-
-
-];
-
-// console.log(bikesArray);
-
-// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal nel DOM
-
-// creo una variabile assumendo che la prima dell'indice sia quella con peso minore ----> 
-    // se il peso di thisBikes < lessBikeWeight 
-    // allora bikeWeight = thisBikes
-
-let lessWeightBike = bikesArray[0];
-// console.log(lessWeightBike);
-
-
-// creo il ciclo for per scorrere l'array con tutti gli objects al suo interno
-
-for(let i = 0; i < bikesArray.length;i++){
-    // mi salvo l'indice dell'array in una variabile
-    let thisBikes = bikesArray[i];
-    // console.log(thisBikes);
-   
-
-    // stabilisco con una if quale bici è più leggera (quella che io ho stabilito che lo sia confrontandola con tutte quelle presenti nell'array)
-
-    if(thisBikes.pesoKg < lessWeightBike.pesoKg){
-        lessWeightBike = thisBikes;
-    }
-     console.log(lessWeightBike);
+students.forEach((elements,index,array) => {
+    let name = elements.name.toUpperCase();
     
+    const newStudent ={
+        name
+    };
+    // console.log(newStudent);
+    provaArray.push(newStudent);
+});
+console.log(provaArray);
+
+
+// versione con MAp
+
+
+ const targStudents = students.map((elements,index,array) => {
+     // console.log(elements);
+     let uppernames = elements.name.toUpperCase();
+     console.log(uppernames);
+
+     return uppernames;
     
-};
-    
-     console.log(lessWeightBike);
+ });
 
-    //  CREO I NNUOVI ELEMENTI HTML E LI POPOLO (versione senza destrutturazione)
-     const htmlContainer = document.querySelector('.text-container');
-    //  console.log(htmlContainer);
+ console.log(targStudents);
 
-    //  let htmlContent = `
-    //  <h2>nome: [${lessWeightBike.nome}]</h2>
-    //  <h3>peso: [${lessWeightBike.pesoKg}]</h3>
-    //  `;
-    
-    // VERSIONE CON DESTRUTTURAZIONE
-    const {nome,pesoKg}=lessWeightBike;
+// 2----------- creo un nuovo array con filter con cui appunto,filtrerò la chiave grades
 
-      let htmlContent = `
-       <h2>nome: [${nome}]</h2>
-       <h3>peso: [${pesoKg}]</h3>
-      `;
+const gradesArray = students.filter((elements,index,array) => {
 
-     
+    return elements.grades >= 70
 
-     htmlContainer.innerHTML += htmlContent;
+});
+console.log(gradesArray);
+
+// 3-  Dobbiamo creare un nuovo array di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120  (filter)
+
+const eliteStudents = students.filter((elements,index,array) => {
+
+    return elements.grades > 70 && elements.id > 120
+
+});
+
+console.log(eliteStudents);
+
